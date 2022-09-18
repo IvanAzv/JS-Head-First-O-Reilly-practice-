75426adc0049f331fd5ebafed9be193550a723cf
IvanAzv <ivan.azvolinskiy@gmail.com>
2022-09-18 17:45:19 +0300

    second variant

--- section3_functions/func_lesson_1.2.js
+++ section3_functions/func_lesson_1.2.js
@@ -1,6 +1,6 @@
 function calculateArea(r) {
     let area;
-    if (r<=0) {
+    if (r<=0 || isNaN(r)) {
         return 0;
     } else {
         area = Math.PI * r * r;
@@ -9,6 +9,7 @@ function calculateArea(r) {
 }
 
 userAnswer = prompt('Будь ласка, введіть число', 'від 0 до 99');
+
 console.log(calculateArea(userAnswer)); // для перевірки з алерт
 progDecision = calculateArea(userAnswer);
 alert('The area is: ' + progDecision);
@@ -20,3 +21,20 @@ alert('The area is: ' + progDecision);
 // console.log(calculateArea(5));
 // x = +prompt();
 // console.log(calculateArea(x))
+
+
+// інший варіант коду цієї программи, без команди return
+
+function calculateArea2(t) {
+    let area2;
+    if (t<=0) {
+        alert('Будь ласка введіть корректне значення!!')
+    } else {
+        area2 = Math.PI * t * t;
+        console.log(area2);
+        alert(area2);
+    }
+    
+}
+
+calculateArea2(prompt());  
